3e2c402af08b2c4e3ade7d94b95377864ff0f505
ziejaCode <ziejaCode@gmail.com>
2021-07-17 17:02:36 +0100

    initial commit

---
+++ src/main/java/ziejacode/spring_beer_browery/SpringBeerBroweryApplication.java
@@ -0,0 +1,13 @@
+package ziejacode.spring_beer_browery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeerBroweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeerBroweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ziejacode/spring_beer_browery/SpringBeerBroweryApplicationTests.java
@@ -0,0 +1,13 @@
+package ziejacode.spring_beer_browery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeerBroweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
